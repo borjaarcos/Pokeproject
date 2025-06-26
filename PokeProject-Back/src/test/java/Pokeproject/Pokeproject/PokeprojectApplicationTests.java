69957a3eb8f3784a6e6432d1985fea6d0254b968
borja <borja.arcos13@gmail.com>
2025-06-26 13:38:52 +0200

    Back and front init creation

---
+++ PokeProject-Back/src/test/java/Pokeproject/Pokeproject/PokeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package Pokeproject.Pokeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PokeprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
